b5ac746d380b32c94204fa2a39880561ecf27c4d
VictorGarcia2 <victrgarciaphoto@gmail.com>
2025-01-23 13:51:46 -0600

    actualizacion de el componente profile-img

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import Profile from './components/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-</body>
+    <>
+    <Profile />
+    </>
   )
 }
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <>
+    <div className="flex flex-col justify-start text-start gap-11">
+
+      <div className=" w-full  border-[#20293A] bg-[#20293A] p-14 rounded-[80px]">
+        <img
+          className="w-full  rounded-[60px]"
+          src="/public/Github.svg"
+          alt=""
+        />
+      </div>
+      <div>
+        <p className="font-bold  text-6xl text-[#CDD5E0] ">Github</p>
+        <p className="text-5xl text-[#CDD5E0]">How people build software</p>
+      </div>
+    </div>
+    </>
+  );
 }
